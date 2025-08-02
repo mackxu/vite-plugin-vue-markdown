fa7e46a800f106a4d1a85cc74099c95eb00e580a
duoduoxu <duoduoxu@tencent.com>
2025-08-02 14:56:23 +0800

    feat: update doc

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from 'vite';
-
-interface MarkdownPluginOptions {
-    theme?: string;
-}
-
-declare function markdownPlugin(options?: MarkdownPluginOptions): Plugin;
-
-export { markdownPlugin };
